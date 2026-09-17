29ac641a50413325d14908e219f1fcab4e948260
Verónica Vanesa García Zarate <zs24016699@estudiantes.uv.mx>
2026-09-17 17:19:57 -0600

    refactor: render auth panel from app header

--- vani/src/main.jsx
+++ vani/src/main.jsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider.jsx'
-import AuthPanel from './auth/AuthPanel.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
-      <AuthPanel />
     </AuthProvider>
   </StrictMode>,
 )
